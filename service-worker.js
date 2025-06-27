25a1e6b7f120abe71a4818da8c625c7e19eb8f8d
bangdit28 <adityahikmahtiar866@gmail.com>
2025-06-28 05:31:03 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'name-generator-cache-v1';
+const urlsToCache = [
+  './generator.html', // File utama
+  './manifest.json', // File manifest
+  'https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Poppins:wght@400;600;700&display=swap', // Font
+  'https://i.imgur.com/biPfr2r.png' // Logo
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
