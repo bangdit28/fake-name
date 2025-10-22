3c7efb7bd0186ff5ae0b757936fdedc5d26eb854
bangdit28 <adityahikmahtiar866@gmail.com>
2025-10-23 01:00:18 +0700

    Update and rename database-nama.js to data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const DATA = {
+    fr: {
+        female: [
+            // Lo bisa tambahin nama baru di sini, bro!
+            "Marie Martin", "Emma Bernard", "Louise Dubois", "Jade Thomas", "Chloé Robert", "Manon Leroy", "Alice Moreau", "Camille Simon",
+            "Lucette Leduc", "Athenais David", "Jeanette Bouchard", "Idelle Molyneux", "Henriette Lefèbvre",
+            "Megane Gagnon", "Doriane Petit", "Claire Broussard", "Ann Charbonneau", "Léonne Jacqui Moreau",
+            "Suzette Abelle Robert", "Jeannette Leroy", "Sandrine Renard", "Odile Blanc", "Marielle Cloutier",
+            "Catharine Prideux", "Seraphine Lefevre", "Michelle Bonnet", "Martine Bertrand", "Cleménce Lambert",
+            "Magalie Houde", "Nadine Vallée", "Melisent Béatrice Paquet", "Gilbertine Bélanger", "Monique Bergeron",
+            "Deline Caron", "Julie Gaudet", "Fleur Gravois", "Lottie Ouellet", "éloise Boucher"
+        ]
+    },
+    emailFakeDomains: [
+        "akbip.com", "bytonf.com", "jiminfunds.com", "doremifasoleando.es", "pickuplanet.com", "xdigit.top", "helenk.site", 
+        "hieuclone.com", "nowfitpro.com", "besnetor.com", "izhowto.com", "edgenestlab.com", "kliknesia.io", "modujoa.com", 
+        "dishow.net", "viavuive.net", "tracklacker.com", "cayxupro5.com", "maxseeding.com", "vinaclicks.com", "newgoldkey.com", 
+        "edgepodlab.com", "chaocosen.com", "questionabledahuli.io", "tiktokngon.com", "btcmod.com", "primejetnet.com", 
+        "maxseeding.vn", "tirasya.pro", "codemail1.com", "pdood.com", "cosoinan.com"
+    ]
+};
